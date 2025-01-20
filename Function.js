67a1ee88e48f3ec745d86523b9b366707ab83c00
lucky <luckydamamii2410@gmail.com>
2025-01-20 12:21:51 +0530

    Add some functionalities

--- Function.js
+++ Function.js
@@ -1,13 +1,29 @@
 // education script work
+const educationPanel = document.querySelector("h2");
 const educationTab = document.querySelector(".education-tab");
 const tabContents = document.querySelector("#education");
 
-educationTab.addEventListener("click", () => {
+educationPanel.addEventListener("click", () => {
   tabContents.classList.toggle("active-tab");
 
-  educationTab.style.backgroundColor = tabContents.classList.contains(
-    "active-tab"
-  )
-    ? "#e3f2fd"
-    : "#f3f3f3";
+  educationTab.style.boxShadow = tabContents.classList.contains("active-tab")
+    ? "0 6px 10px rgba(0, 0, 0, 0.3)"
+    : "none";
+});
+
+/* View Certificate Functionality*/
+const certificateLinks = document.querySelectorAll(".certificate");
+
+certificateLinks.forEach((link, index) => {
+  link.addEventListener("click", () => {
+    // Map the links to respective PDF URLs
+    const certificateURLs = [
+      "",
+      "certificates/bsc-certificate.pdf",
+      "certificates/12th-certificate.pdf",
+    ];
+
+    // Open the respective certificate in a new tab
+    window.open(certificateURLs[index], "_blank");
+  });
 });
